7026b517b3cd16321b6e3cdc35b5abfec5f90be1
Marc <recatala.marc@gmail.com>
2018-07-12 18:35:07 +0200

    Worked on fixing the bug on tests.

--- src/utils/findRandomUser.js
+++ src/utils/findRandomUser.js
@@ -1,33 +1,22 @@
 const
 	User = require( "../models/User" );
 
-findRandomUser = userId => {
-	return new Promise( function( resolve, reject ) {
-		loop = () => {
-			User.count()
-				.exec().then( count => {
-					const randomNum = Math.floor( Math.random() * count );
-					User.findOne()
-						.skip( randomNum )
-						.select(
-							"username fullname description posts keywords profileImage " +
-							"headerImage friends followers"
-						)
-						.exec()
-						.then( user => {
-							// if the returned user is not the requester resolve
-							// else get another one
-							if ( user.id !== userId ) {
-								resolve( user );
-							} else {
-								loop();
-							}
-						})
-						.catch( err => reject( err ));
-				}).catch( err => reject( err ));
-		};
-		loop();
-	});
+findRandomUser = async userId => {
+	try {
+		const
+			count = await User.count().exec(),
+			randomNum = Math.floor( Math.random() * count ),
+			user = await User.findOne()
+				.skip( randomNum )
+				.select(
+					"username fullname description posts keywords profileImage " +
+					"headerImage friends followers"
+				)
+				.exec();
+		return user;
+	} catch ( err ) {
+		throw err;
+	}
 };
 
 module.exports = findRandomUser;
